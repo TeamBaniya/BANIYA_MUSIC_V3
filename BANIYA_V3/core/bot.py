2e23b7c429b2c9faf78236e2d1877401b02ed3c0
ꜱᴘᴧʀꜱʜ ʙᴧɴɪʏᴧ <89186991+TeamBaniya@users.noreply.github.com>
2026-05-11 00:29:15 +0530

    Update bot.py

--- BANIYA_V3/core/bot.py
+++ BANIYA_V3/core/bot.py
@@ -2,31 +2,35 @@
 # Licensed under the MIT License.
 # This file is part of AnonXMusic
 
-from pyrogram import Client, filters, enums
+
+import pyrogram
+
 from BANIYA_V3 import config, logger
 
 
-class Bot(Client):
+class Bot(pyrogram.Client):
     def __init__(self):
         super().__init__(
             name="BANIYA_V3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            parse_mode=enums.ParseMode.HTML,
+            parse_mode=pyrogram.enums.ParseMode.HTML,
             max_concurrent_transmissions=7,
-            disable_web_page_preview=True,
+            link_preview_options=pyrogram.types.LinkPreviewOptions(is_disabled=True),
         )
         self.owner = config.OWNER_ID
         self.logger = config.LOGGER_ID
-        self.bl_users = filters.user()
-        self.sudoers = filters.user(self.owner)
-        self.id = None
-        self.name = None
-        self.username = None
-        self.mention = None
+        self.bl_users = pyrogram.filters.user()
+        self.sudoers = pyrogram.filters.user(self.owner)
 
     async def boot(self):
+        """
+        Starts the bot and performs initial setup.
+
+        Raises:
+            SystemExit: If the bot fails to access the log group or is not an administrator in the logger group.
+        """
         await super().start()
         self.id = self.me.id
         self.name = self.me.first_name
@@ -34,16 +38,18 @@ class Bot(Client):
         self.mention = self.me.mention
 
         try:
-            await self.send_message(self.logger, "✅ Bot Started Successfully!")
+            await self.send_message(self.logger, "Bot Started")
             get = await self.get_chat_member(self.logger, self.id)
         except Exception as ex:
-            raise SystemExit(f"❌ Bot failed to access log group: {self.logger}\nReason: {ex}")
+            raise SystemExit(f"Bot has failed to access the log group: {self.logger}\nReason: {ex}")
 
-        if get.status != enums.ChatMemberStatus.ADMINISTRATOR:
-            raise SystemExit("❌ Please promote bot as admin in logger group!")
-        
-        logger.info(f"✅ Bot started as @{self.username}")
+        if get.status != pyrogram.enums.ChatMemberStatus.ADMINISTRATOR:
+            raise SystemExit("Please promote the bot as an admin in logger group.")
+        logger.info(f"Bot started as @{self.username}")
 
     async def exit(self):
+        """
+        Asynchronously stops the bot.
+        """
         await super().stop()
-        logger.info("✅ Bot stopped.")
+        logger.info("Bot stopped.")
